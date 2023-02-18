c4db87ae9d40b9a5d6fbe62b19ce19cd4e21c712
L3o <74620207+leorchr@users.noreply.github.com>
2023-02-18 15:45:16 +0100

    Update Transitions Collisions

--- Assets/Data/Gabriel-Guilhem-Leo/Scripts/CameraManager.cs
+++ Assets/Data/Gabriel-Guilhem-Leo/Scripts/CameraManager.cs
@@ -8,7 +8,13 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private SpringArm springArm;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Fps"))
+        if(springArm.cameraStatus == CameraStatus.FirstPerson ||
+            springArm.cameraStatus == CameraStatus.ThirdPersonClose ||
+            springArm.cameraStatus == CameraStatus.ThirdPersonFar)
+        {
+            springArm.cameraStatus = CameraStatus.ThirdPerson;
+        }
+        else if (other.CompareTag("Fps"))
         {
             springArm.cameraStatus = CameraStatus.FirstPerson;
         }
@@ -19,18 +25,6 @@ public class CameraManager : MonoBehaviour
         else if (other.CompareTag("TpsFar"))
         {
             springArm.cameraStatus = CameraStatus.ThirdPersonFar;
-        }
-        else if (other.CompareTag("Tps"))
-        {
-            springArm.cameraStatus = CameraStatus.ThirdPerson;
-        }
-
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Fps") || other.CompareTag("TpsClose") || other.CompareTag("TpsFar"))
-        {
-            springArm.cameraStatus = CameraStatus.ThirdPerson;
         }
     }
 }
