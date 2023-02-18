1afd15a268a99c3f7aa7c90efc2025ba80af58ba
L3o <74620207+leorchr@users.noreply.github.com>
2023-02-18 14:37:53 +0100

    smooth damp player rotation

--- Assets/Scripts/Marie/Character/PlayerController.cs
+++ Assets/Scripts/Marie/Character/PlayerController.cs
@@ -1,11 +1,13 @@
 using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _movementSpeed = 5, _rotationSpeed = 100;
+    [SerializeField] private float _movementSpeed = 5, _rotationSpeed = 100, _smoothRotation = 0.2f;
     private Vector2 _movement = Vector2.zero;
+    private Vector3 rotVelocity;
     private Animator _animator;
     private Rigidbody _rigidbody;
     void Start()
@@ -18,7 +20,9 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 cameraDir = Camera.main.transform.forward;
         cameraDir.y = 0f;
-        transform.forward = cameraDir;
+        //transform.forward = cameraDir;
+        transform.forward = Vector3.SmoothDamp(transform.forward, cameraDir, ref rotVelocity, _smoothRotation);
+
         //transform.Rotate(0, _movement.x * Time.deltaTime * _rotationSpeed, 0);
         _rigidbody.MovePosition(transform.position + transform.forward * (_movement.y * _movementSpeed * Time.deltaTime));
         //transform.position += transform.forward * (_movement.y * _movementSpeed * Time.deltaTime);
